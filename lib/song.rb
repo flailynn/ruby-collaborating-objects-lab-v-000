efe2759666e6946afdd4aa79b48620e73fdb3811
Anita Beauchamp <amadamspr@gmail.com>
2018-07-29 21:10:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,5 +8,14 @@ class Song
 
   def self.new_by_filename(filename)
     song = Song.new(filename)
+
+    #For each file
+    file_array.each do |filename|
+      #Chop .mp3 of the name
+      filename.chomp!(".mp3")
+
+      #Split filename (string) into array
+      song_details = filename.split(" - ")
+
   end
 end
