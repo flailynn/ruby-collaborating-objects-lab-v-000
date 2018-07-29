c9b83edd343b32ed2a3fb583efaf9f7492f8706d
Anita Beauchamp <amadamspr@gmail.com>
2018-07-29 20:36:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,10 @@ class Song
 
   attr_accessor :name, :artist
 
-  def initialize(name)
-    self.name = name
-  end
-  
+
+
   def self.new_by_filename(filename)
-    Song.new(filename)
+    song = Song.new()
+    song.name = filename
   end
 end
