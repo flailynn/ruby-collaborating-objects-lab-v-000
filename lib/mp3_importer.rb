6c600df42b679c787fd74bd661d2086631dc1d36
Anita Beauchamp <amadamspr@gmail.com>
2018-07-29 21:30:20 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,7 +13,7 @@ class MP3Importer
     #Change directory to the filepath provided on initialization
     Dir.chdir(self.path) do
       #Grab all files in the directory that are mp3 format
-      self.files = Dir.glob(self.path + "*.mp3")
+      self.files = Dir.glob("*.mp3")
     end
   end
 
