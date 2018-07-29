058e9fd5482138ff3e3ec4f41c502dbd55450df1
Anita Beauchamp <amadamspr@gmail.com>
2018-07-29 20:16:01 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,17 @@
 class Artist
   attr_accessor :name, :songs
 
+  @@all = []
+  
   def initialize(name)
     self.name = name
     self.songs = []
   end
 
+  def save
+    @@all << self
+  end
+  
   def self.all
     @@all
   end
