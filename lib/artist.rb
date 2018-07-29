486a2fc3bc3ae52a19caccc656582ec2281b1834
Anita Beauchamp <amadamspr@gmail.com>
2018-07-29 20:31:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,6 +13,12 @@ class Artist
     @@all << self
   end
 
+  def print_songs
+    self.songs.each do |song|
+      puts song.name
+    end
+  end
+
   def self.find_or_create_by_name(name)
     artist = self.all.find {|artist| artist.name == name}
     artist ? artist : Artist.new(name)
